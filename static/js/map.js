33f45f4a999e62113405a0ac15c4e569c2d5c162
Harvey Xia <harvey@harveyxia.com>
2019-07-10 12:54:06 -0400

    Harcode hack y positioning of map.

--- static/js/map.js
+++ static/js/map.js
@@ -1,14 +1,12 @@
 var height = 1000
 var width = 1000
 
-console.log(d3.select('#map').node())
-
 var svg = d3.select('#map')
   .append("svg")
   .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", `0 0 ${height} ${width}`);
+  .attr("viewBox", `0 180 ${height} ${width}`);
 var g = svg.append('g');
-var projection = d3.geoAlbers();
+var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
 d3.json('/json/countries.topo.json', function(error, countriesTopojson) {
@@ -20,5 +18,8 @@ d3.json('/json/countries.topo.json', function(error, countriesTopojson) {
     .data(geojson.features)
     .enter()
     .append("path")
-    .attr('d', path);
+    .attr('d', path)
+    .style("fill", "steelblue")
+    .style("stroke-width", "1")
+    .style("stroke", "white");
 })
