362e4b2f04d405170c07c2ca21b1acedcdd1885c
Harvey Xia <harvey@harveyxia.com>
2019-07-10 20:36:21 -0400

    Constrain zoom and pan.g

--- static/js/map.js
+++ static/js/map.js
@@ -3,22 +3,24 @@ var width = 2000
 
 var svg = d3.select('#map')
   .append('svg')
-  // .attr('preserveAspectRatio', 'xMinYMin meet')
-  .attr('viewBox', `0 360 ${height} ${width}`)
-  .call(d3.zoom().on("zoom", function() {
-    svg.attr("transform", d3.event.transform)
-  }))
-  .append('g');
+  .attr('viewBox', `0 300 ${height} ${width}`)
+  .call(
+    d3.zoom()
+    .on("zoom", function() {
+      d3.event.transform.x = Math.min(0, Math.max(d3.event.transform.x, width - width * d3.event.transform.k))
+      d3.event.transform.y = Math.min(0, Math.max(d3.event.transform.y, height - height * d3.event.transform.k))
+      svg.attr("transform", d3.event.transform)
+    })
+    .scaleExtent([1, 5])
+  ).append('g');
 
 var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
 d3.json('/json/countries.topo.json', function(error, countriesTopojson) {
   if (error) throw error;
-
   var geojson = topojson.feature(countriesTopojson, countriesTopojson.objects.countries)
   projection.fitSize([width, height], geojson)
-
   // draw map
   svg.selectAll('path')
     .data(geojson.features)
@@ -28,7 +30,6 @@ d3.json('/json/countries.topo.json', function(error, countriesTopojson) {
 });
 
 d3.json('/json/places.json', function(error, places) {
-  // add circles to svg
   svg.selectAll("circle")
     .data(places)
     .enter()
@@ -42,22 +43,3 @@ d3.json('/json/places.json', function(error, places) {
     .attr("r", "8px")
     .attr("fill", "red")
 })
-
-// Copied from https://bl.ocks.org/piwodlaiwo/7dac64184e3581d2e7a6d9c3220aa958
-var drag = d3.drag()
-  .on("start", function() {
-    // Adapted from http://mbostock.github.io/d3/talk/20111018/azimuthal.html and updated for d3 v3
-    var proj = projection.rotate();
-    m0 = [d3.event.sourceEvent.pageX, d3.event.sourceEvent.pageY];
-    o0 = [-proj[0], -proj[1]];
-  })
-  .on("drag", function() {
-    if (m0) {
-      var m1 = [d3.event.sourceEvent.pageX, d3.event.sourceEvent.pageY],
-        o1 = [o0[0] + (m0[0] - m1[0]) / 4, o0[1] + (m1[1] - m0[1]) / 4];
-      projection.rotate([-o1[0], -o1[1]]);
-    }
-    // Update the map
-    path = d3.geoPath().projection(projection);
-    d3.selectAll("path").attr("d", path);
-  });
